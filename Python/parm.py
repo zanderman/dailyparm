ced72dada58d0c134d948f8c692d4c122532fa84
Alexander Derieux <derieux@Administrators-MacBook-Pro.local>
2016-09-01 01:03:01 -0400

    Scrapes the current Owens menu for Chicken Parmesan.

---
+++ Python/parm.py
@@ -0,0 +1,46 @@
+
+##########################################################
+# Chicken Parmesan Checker
+#
+# Description:
+# 	Curious if it's chicken parmesan day at Owen's?
+# 	Well, this tool is for you! This tool will check
+# 	the current date and then tell you if it is chicken
+# 	parmesan day.
+#
+##########################################################
+import requests
+from BeautifulSoup import BeautifulSoup
+
+
+def parm():
+
+	# Link to the Owen's menu website.
+	URL = "http://foodpro.dsa.vt.edu/FoodPro.NET/shortmenu.aspx?sName=Virginia+Tech+Dining+Services&locationNum=09&locationName=FOOD+CRT%2f+HOKIE+GRILL+AT+OWENS+&naFlag=1"
+	# "http://foodpro.dsa.vt.edu/FoodPro.NET/shortmenu.aspx?sName=Virginia+Tech+Dining+Services&locationNum=09&locationName=FOOD+CRT%2f+HOKIE+GRILL+AT+OWENS+&naFlag=1&WeeksMenus=This+Week%27s+Menus&myaction=read&dtdate=8%2f30%2f2016"
+	response = requests.get(URL)
+	html = response.content
+
+	# Run the HTML content through BeautifulSoup
+	soup = BeautifulSoup(html)
+
+	# Iterate over all meals for the day at Owen's
+	for meal in soup.findAll('div', attrs={'class': 'meal_listing_item'}):
+		
+		# Get all recipes
+		for recipe in meal.findAll('div', attrs={'class': 'shortmenurecipes'}):
+
+			# Get all links associated with the current recipe
+			for link in recipe.findAll('a'):
+
+				# Check if the text of the link contains wording for Chicken Parmesan
+				if "chicken parmesan" in link.contents[0].lower():
+					print "Yes, today is Chicken Parmesan day." # prints name of meal!
+					return True
+
+	# No parmesan was found.
+	print "No, today is not Chicken Parmesan day."
+	return False
+
+if __name__ == "__main__":
+	parm()
